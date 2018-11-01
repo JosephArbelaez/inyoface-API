2ea694b71c6da4a295f4f2dc3f252541c1749c9e
Joseph Arbelaez <josepharbelaez@gmail.com>
2018-11-01 16:11:29 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user: 'vn503o9',
+        password : '',
+        database : 'inyoface'
+    }
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:userId', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res, db)})
