a05cfdcd7f961c998e51f009d8bc99bbabeb73b0
Joseph Arbelaez <josepharbelaez@gmail.com>
2018-11-02 09:20:18 -0500

    Andrei Changes

--- controllers/image.js
+++ controllers/image.js
@@ -6,14 +6,14 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
     app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
         .then(data => {
             res.json(data);
         })
         .catch(err => res.status(400).json('Unable to work with API'))
       }
 
-const handelImage = (req, res, db) => {
+const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
         .increment('entries', 1)
